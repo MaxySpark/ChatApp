55ac151281fc7e943a631a8eb8c42370a1e5128a
MaxySpark <devmaxyspark@gmail.com>
2018-07-17 02:28:46 +0530

    signup

--- controller/users.js
+++ controller/users.js
@@ -1,12 +1,16 @@
 'user strict';
 
-module.exports = function(_) {
+module.exports = function() {
     return {
         setRouting : function(router) {
             router.get('/',this.indexPage);
+            router.get('/signup',this.getSignUp);
         },
         indexPage : function(req,res) {
             return res.render('index',{test : 'this is a test'});
+        },
+        getSignUp : function(req,res) {
+            return res.render('signup');
         }
     }
 }
--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const passport = require('passport');
+
 const container = require('./container');
 
 container.resolve(function(users){
@@ -27,10 +35,25 @@ container.resolve(function(users){
 
 
     function configureExpress(app) {
+        mongoose.Promise = global.Promise;
+        const mongoDB = 'mongodb://localhost:27017/chatapp';
+        mongoose.connect(mongoDB,{ useNewUrlParser: true });
         app.use(express.static('public'));
+        app.use(cookieParser());
         app.set('view engine','ejs');
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended : true}));
+
+        app.use(validator());
+        app.use(session({
+            secret : 'abcdefgh',
+            resave : true,
+            saveUninitialized : true,
+            store : new MongoStore({mongooseConnection : mongoose.connection})
+        }));
+        app.use(flash());
+        app.use(passport.initialize());
+        app.use(passport.session());
     }
         
 
