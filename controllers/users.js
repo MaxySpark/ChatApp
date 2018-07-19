7eea330aea753ff4892bb8ca0435bc0f878b59ed
MaxySpark <devmaxyspark@gmail.com>
2018-07-20 02:03:31 +0530

    login signup

--- controllers/users.js
+++ controllers/users.js
@@ -5,18 +5,26 @@ module.exports = function(_,passport,UserHelper) {
             router.get('/',this.indexPage);
             router.get('/signup',this.getSignUp);
             router.get('/home',this.homePage);
+
+            router.post('/', UserHelper.loginValidation ,this.postLogin);
             router.post('/signup',UserHelper.signUpValidation ,this.postSignUp);
         },
         indexPage : function(req,res) {
-            return res.render('index',{test : 'this is a test'});
+            const errors = req.flash('error');
+            return res.render('index', { title : 'Chat App | Login', messages : errors , hasError : (errors.length > 0) });
         },
         getSignUp : function(req,res) {
             const errors = req.flash('error');
-            return res.render('signup', { title : 'Chat App | Login', messages : errors , hasError : (errors.length > 0) });
+            return res.render('signup', { title : 'Chat App | SignUp', messages : errors , hasError : (errors.length > 0) });
         },
         postSignUp : passport.authenticate('local.signup', {
             successRedirect : '/home',
             failureRedirect : '/signup',
+            failureFlash : true
+        }),
+        postLogin : passport.authenticate('local.login', {
+            successRedirect : '/home',
+            failureRedirect : '/',
             failureFlash : true
         }),
         homePage : function(req,res) {
--- helpers/UserHelper.js
+++ helpers/UserHelper.js
@@ -24,6 +24,27 @@ module.exports = function() {
                 .catch((err) => {
                     return next();
                 });
+        },
+        loginValidation : (req, res, next) => {
+            req.checkBody('email','Email is required').notEmpty();
+            req.checkBody('email','Email is invalid').isEmail();
+            req.checkBody('password','Password is required').notEmpty();
+            req.checkBody('password','Password must not be less than 8').isLength({min : 8});
+
+            req.getValidationResult()
+                .then((result) => {
+                    const errors = result.array();
+                    const messages = [];
+                    errors.forEach(error => {
+                        messages.push(error.msg);
+                    });
+
+                    req.flash('error',messages);
+                    res.redirect('/');
+                })
+                .catch((err) => {
+                    return next();
+                });
         }
     }
 };
